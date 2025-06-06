9b30b1256747d3b0a5a73ece47db4fcddf43667a
Paul Gaiduk <paulg@zededa.com>
2025-06-06 13:01:54 +0200

    more vector improvements
    Signed-off-by: Paul Gaiduk <paulg@zededa.com>

--- pkg/vector/entrypoint.sh
+++ pkg/vector/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 mkdir -p /persist/vector
+
+export VECTOR_LOG="vector=info,vector::sources::util::unix_stream=warn"
+export VECTOR_LOG_FORMAT="json"
+
 # vector -w 2>&1 | tee -a /persist/vector/vector.log
 vector -w >> /persist/vector/stdout.log 2>> /persist/vector/stderr.log
